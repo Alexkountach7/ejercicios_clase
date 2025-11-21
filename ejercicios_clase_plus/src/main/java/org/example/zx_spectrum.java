5e1ea9b42a8a0ea5f4d7f4dc0dee4f452f6427d8
A614\1DAW <alexkountach7@gmail.com>
2025-11-21 17:19:28 +0100

    ejercicios

--- ejercicios_clase_plus/src/main/java/org/example/zx_spectrum.java
+++ ejercicios_clase_plus/src/main/java/org/example/zx_spectrum.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class zx_spectrum {
     static void main() {
         Scanner sc = new Scanner(System.in);
-        
+
 
         System.out.println("**** VALIDADOR DE COMPATIBILIDAD ZX SPECTRUM *****");
         System.out.println("ancho:");
